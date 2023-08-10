7f5dd82aa49190843e38c925b84796429e0685f3
B Laureen <beffylaureen@me.com>
2023-08-09 21:42:34 -0500

    commit tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,22 +3,59 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (wish, gift) => {
   // find all tags
   // be sure to include its associated Product data
-});
+  try{
+    const productSKU=await Tag.findAll({
+      include:[{Product}]
+    });
+    gift.status(200).json(productSKU);
+  }catch(shrink){
+    gift.status(500).json(shrink);
+  }
+ });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(wish, gift) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const productSKU=await Tag.findByPk(wish.params.id,{
+      include:[{model:Product}]
+    });
+    if(!productSKU){
+      gift.status(404).json({message: 'item not available'});
+      return;
+    }
+    gift.status(200).json(productSKU);
+  }catch(shrink){
+    gift.status(500).json(shrink);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (wish, gift) => {
   // create a new tag
+  try{
+    const productSKU=await Tag.create(wish.body);
+    gift.status(200).json(productSKU);
+  }catch(shrink){
+    gift.status(400).json(shrink);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(order, package) => {
   // update a tag's name by its `id` value
+  try{
+    const newTag=await Tag.update(order.body.{
+      where:{
+        id:order.params.id,
+      }
+    })
+    console.log(newTag);
+    package.status(200).json()
+  }catch(shrink){
+    package.status(500).json(shrink.message)
+  }
 });
 
 router.delete('/:id', (req, res) => {
